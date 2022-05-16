755fdb904bbd35406a7f1397fb5f1504f439f0c5
07akioni <07akioni2@gmail.com>
2022-05-17 03:21:30 +0800

    feat: make it work

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -1,17 +1,31 @@
 import { setup } from "@css-render/vue3-ssr";
 import { defineNuxtPlugin } from "#app";
 
-// I'm not sure whether the plugin is called twice in each refreshing
-// Maybe it's expected. If you have more information about it, please comment in the issue
 export default defineNuxtPlugin((nuxtApp) => {
-  // if (process.server) {
+  if (process.server) {
     const { collect } = setup(nuxtApp.vueApp);
-    const originalRender = nuxtApp.ssrContext.renderMeta;
-    nuxtApp.ssrContext!.renderMeta = () => {
-      const result = originalRender();
-      return {
-        headTags: result["headTags"] + collect(),
-      };
+    const originalRenderMeta = nuxtApp.ssrContext?.renderMeta;
+    nuxtApp.ssrContext = nuxtApp.ssrContext || {};
+    nuxtApp.ssrContext.renderMeta = () => {
+      if (!originalRenderMeta) {
+        return {
+          headTags: collect(),
+        };
+      }
+      const originalMeta = originalRenderMeta();
+      if ("then" in originalMeta) {
+        return originalMeta.then((resolvedOriginalMeta) => {
+          return {
+            ...resolvedOriginalMeta,
+            headTags: resolvedOriginalMeta["headTags"] + collect(),
+          };
+        });
+      } else {
+        return {
+          ...originalMeta,
+          headTags: originalMeta["headTags"] + collect(),
+        };
+      }
     };
-  // }
+  }
 });
