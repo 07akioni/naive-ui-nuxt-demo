b0ceef3d821ac38ba6bbd7ed5923b420aa38c828
Jimmyzm <jim_zm@163.com>
2022-12-01 16:43:08 +0800

    modify nuxt type

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -1,11 +1,11 @@
 import { setup } from '@css-render/vue3-ssr'
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, NuxtSSRContext } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.server) {
     const { collect } = setup(nuxtApp.vueApp)
     const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
-    nuxtApp.ssrContext = nuxtApp.ssrContext || {}
+    nuxtApp.ssrContext = nuxtApp.ssrContext || ({} as NuxtSSRContext)
     nuxtApp.ssrContext.renderMeta = () => {
       if (!originalRenderMeta) {
         return {
