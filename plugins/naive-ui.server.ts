d7f5868ab392920b0364f8e007c02b2447990879
becem-gharbi <becem.gharbi@live.com>
2023-11-17 11:23:49 +0100

    fix: Add support for nuxt >=3.7

--- plugins/naive-ui.server.ts
+++ plugins/naive-ui.server.ts
@@ -1,30 +1,18 @@
 import { setup } from '@css-render/vue3-ssr'
 
+// Credit @harlan-zw
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
-    const { collect } = setup(nuxtApp.vueApp)
-    const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
-    nuxtApp.ssrContext = nuxtApp.ssrContext || {}
-    nuxtApp.ssrContext.renderMeta = () => {
-      if (!originalRenderMeta) {
+  const { collect } = setup(nuxtApp.vueApp);
+  nuxtApp.ssrContext!.head.push({
+    style: () => collect()
+      .split('</style>')
+      .map((block) => {
+        const id = block.match(/cssr-id="(.+?)"/)?.[1]
+        const style = (block.match(/>(.*)/s)?.[1] || '').trim()
         return {
-          headTags: collect()
+          ['cssr-id']: id,
+          innerHTML: style,
         }
-      }
-      const originalMeta = originalRenderMeta()
-      if ('then' in originalMeta) {
-        return originalMeta.then((resolvedOriginalMeta) => {
-          return {
-            ...resolvedOriginalMeta,
-            headTags: resolvedOriginalMeta['headTags'] + collect()
-          }
-        })
-      } else {
-        return {
-          ...originalMeta,
-          headTags: originalMeta['headTags'] + collect()
-        }
-      }
-    }
-  }
+      })
+  })
 })
