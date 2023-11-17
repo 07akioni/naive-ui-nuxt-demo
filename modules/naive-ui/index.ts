c0d7f280871e42aa434ba4f8c792dbc28849201d
becem-gharbi <becem.gharbi@live.com>
2023-11-17 11:33:02 +0100

    chore: Move configuration to module

--- modules/naive-ui/index.ts
+++ modules/naive-ui/index.ts
@@ -1,11 +1,33 @@
-import { createResolver, defineNuxtModule, addPlugin } from 'nuxt/kit'
+import { createResolver, defineNuxtModule, addPlugin, extendViteConfig } from 'nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: 'naive-ui'
   },
-  setup () {
+  setup (options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
     addPlugin(resolve('./runtime/plugin.server.ts'))
-   }
+
+     // https://www.naiveui.com/en-US/os-theme/docs/ssr
+    if (process.env.NODE_ENV === "production") {
+      nuxt.options.build.transpile.push(
+        "naive-ui",
+        "vueuc",
+        "@css-render/vue3-ssr",
+        "@juggle/resize-observer"
+      );
+    } else {
+      nuxt.options.build.transpile.push("@juggle/resize-observer");
+
+      extendViteConfig((config) => {
+        config.optimizeDeps = config.optimizeDeps || {};
+        config.optimizeDeps.include = config.optimizeDeps.include || [];
+        config.optimizeDeps.include.push(
+          "naive-ui",
+          "vueuc",
+          "date-fns-tz/formatInTimeZone"
+        );
+      });
+    }
+  }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,3 @@
  // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    transpile:
-      process.env.NODE_ENV === 'production'
-        ? [
-            'naive-ui',
-            'vueuc',
-            '@css-render/vue3-ssr',
-            '@juggle/resize-observer'
-          ]
-        : ['@juggle/resize-observer']
-  },
-  vite: {
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui', 'vueuc', 'date-fns-tz/formatInTimeZone']
-          : []
-    }
-  }
 })
